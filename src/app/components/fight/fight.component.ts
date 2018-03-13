8e887675b57791903303d565ca8a7a636b784d48
Anar Orujov <anarorujov9@gmail.com>
2018-03-13 16:32:05 +0400

    Completed Character Creation Page section

---
+++ src/app/components/fight/fight.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "fight-component",
+    templateUrl: "./fight.component.css",
+    styleUrls: ["./fight.component.css"]
+})
+export class FightComponent {
+    
+}
